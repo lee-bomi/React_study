f6ae92f3d2fcec4ef228a6f5d413a28ec0e2409d
emmababy <bomvll@naver.com>
2022-03-12 13:15:51 +0900

    end basic course

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
   let [따봉, 따봉변경] = useState(0);
   let [modal, modal변경] = useState(true);
   let [누른제목, 누른제목변경] = useState(0);
+    let [입력값, 입력값변경] = useState('');
 
   function 제목바꾸기() {  //state를 변경하는것은 원래의 array전체를 put으로 덮어버리는것을 의미, 그래서 원래내용과 형식을 가져와서 수정할부분만 작업
     //글제목수정(['여자 코트 추천','남자댕댕이 코트 추천', '강아지 코트 추천']);
@@ -31,21 +32,27 @@ function App() {
       </div>
       <button onClick={ 제목바꾸기 }>정렬</button>
         {
-            글제목.map((글, i)=>{
-                return (
-                    <div className="list">
-                        <h3
-                            onClick={ ()=>{ 누른제목변경(i) }}>
+            글제목.map((글,i)=>{
+                return(
+                    <div className="list" key={i}>
+                        <h3 onClick={()=>{누른제목변경(i)}}>
                             {글}
-                            <span onClick={ () => { 따봉변경(따봉+1)} }>👍</span>
-                            {따봉}
+                            <span onClick={()=>{따봉변경(따봉+1)}}>👍</span>{따봉}
                         </h3>
-                        <p>3월 3일 발행</p>
+                        <p>3월3일</p>
                         <hr/>
                     </div>
                 )
             })
         }
+        <div className="publish">
+            <input onChange={(e) => {입력값변경(e.target.value)}}/>
+            <button onClick={() => {
+                let copyArray = [...글제목];
+                copyArray.unshift(입력값);
+                글제목변경(copyArray);
+            }}>저장</button>
+        </div>
         <button onClick={()=>{modal변경(!modal)}}>버튼</button>
         {
             modal === true
